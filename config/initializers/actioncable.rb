26307d0ca0482d8483437f0c83f71d1c4b9722c9
Charles DuBose <kayakyakr@gmail.com>
2015-10-20 12:31:21 -0500

    adding XOR domain configuration

--- config/initializers/actioncable.rb
+++ config/initializers/actioncable.rb
@@ -1,1 +1,2 @@
 ActionCable.server.config.worker_pool_size = Integer(ENV["CABLE_POOL_SIZE"] || 10)
+ActionCable.server.config.allowed_request_origins = ENV["DOMAINS"].split unless ENV["DOMAINS"].blank?
